7c7465e05d9a9d7db27459940d6583d436d645f9
Lester <keyachi@163.com>
2019-08-04 08:57:33 +0800

    李某人

--- pyg/src/main/java/HelloWorld.java
+++ pyg/src/main/java/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String []args){
-        System.out.println("hello world");
+
+        System.out.println("李某人");
+        System.out.println("李某人");
+        System.out.println("李某人");
+        System.out.println("李某人");
+        System.out.println("李某人");
     }
 }
